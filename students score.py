f8789fb0f2d67f5ab6481891414f217241fb523e
Sahar <sahar.abbasi13@gmail.com>
2025-03-06 02:16:27 +0330

    Add files via upload

---
+++ students score.py
@@ -0,0 +1,11 @@
+
+score= input('enter your score')
+if the score is >=90:
+    print ('excellent')
+elif the score is 89>=score>=70:
+    print ('nice')
+
+elif the score is 69>=score>=50:
+    print ('acceptable')
+else:
+    print('fail')
