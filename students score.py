bb533c724c8255bdf7fa137fed167dda04ddabae
Sahar <sahar.abbasi13@gmail.com>
2025-03-06 03:29:22 +0330

    Update students score.py

--- students score.py
+++ students score.py
@@ -1,10 +1,8 @@
-
-score= input('enter your score')
+score= input('enter your score:')
 if the score is >=90:
     print ('excellent')
 elif the score is 89>=score>=70:
     print ('nice')
-
 elif the score is 69>=score>=50:
     print ('acceptable')
 else:
